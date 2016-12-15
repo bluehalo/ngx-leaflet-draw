fe8c0a69460bd6f26d7d8752e4470b6eedad1a1b
reblace <reblace@gmail.com>
2016-12-15 08:40:42 -0500

    Removing an unused import

--- src/leaflet-draw/core/leaflet-draw.directive.ts
+++ src/leaflet-draw/core/leaflet-draw.directive.ts
@@ -3,7 +3,6 @@ import { Directive, Input, OnChanges, OnInit, SimpleChange } from '@angular/core
 import * as L from 'leaflet';
 
 import { LeafletDirective } from '@asymmetrik/angular2-leaflet';
-import EventHandlerFn = L.EventHandlerFn;
 
 
 @Directive({
